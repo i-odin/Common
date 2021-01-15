514147d7ced37b387c0937b69c2e29c5fd8aef7f
b_a_x <ily-odinokov@yandex.ru>
2021-01-15 22:35:27 +0300

    abstract base id

---
+++ src/Common.Core/Model/HasIdBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common.Core.Model
+{
+    public abstract class HasIdBase<TKey> : IHasId<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public TKey Id { get; set; }
+
+        object IHasId.Id => Id;
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is HasIdBase<TKey> other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return EqualityComparer<TKey>.Default.Equals(Id, other.Id);
+        }
+
+        public static bool operator ==(HasIdBase<TKey> a, HasIdBase<TKey> b)
+        {
+            if (a is null && b is null) return true;
+            if (a is null || b is null) return false;
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(HasIdBase<TKey> a, HasIdBase<TKey> b) => !(a == b);
+
+        public override int GetHashCode() => EqualityComparer<TKey>.Default.GetHashCode(Id);
+    }
+}
---
+++ src/Common.Core/Model/IHasId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Common.Core.Model
+{
+    public interface IHasId
+    {
+        object Id { get; }
+    }
+
+    public interface IHasId<out TKey> : IHasId
+        where TKey : IEquatable<TKey>
+    {
+        new TKey Id { get; }
+    }
+}
