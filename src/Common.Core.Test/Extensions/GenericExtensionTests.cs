ab3af64344acb2cf70ed16bfeeaa82521df0bbbd
b_a_x <ily-odinokov@yandex.ru>
2021-02-05 10:40:05 +0300

    Generic Extensions

---
+++ src/Common.Core.Test/Extensions/GenericExtensionTests.cs
@@ -0,0 +1,35 @@
+﻿using Common.Core.Extensions;
+using Xunit;
+
+namespace Common.Core.Test.Extensions
+{
+    public class GenericExtensionTest
+    {
+        [Fact]
+        public void InTrue()
+        {
+            const EnumTest enumTest = EnumTest.One;
+
+            var result = enumTest.In(EnumTest.One, EnumTest.Two);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void InFalse()
+        {
+            const EnumTest enumTest = EnumTest.One;
+
+            var result = enumTest.In(EnumTest.Two, EnumTest.Three);
+
+            Assert.False(result);
+        }
+
+        private enum EnumTest : byte
+        {
+            One,
+            Two,
+            Three
+        }
+    }
+}
--- src/Common.Core/Extensions/GenericExtensions.cs
+++ src/Common.Core/Extensions/GenericExtensions.cs
@@ -4,10 +4,7 @@ namespace Common.Core.Extensions
 {
     public static class GenericExtensions
     {
-        //TODO: Test
-        public static bool In<T>(this T obj, params T[] values)
-        {
-            return values.Contains(obj);
-        }
+        public static bool In<T>(this T obj, params T[] values) =>
+            values.Contains(obj);
     }
 }
