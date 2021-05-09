4732143ff638bcdf0bb60a4c5af8eef2873340e2
b_a_x <ily-odinokov@yandex.ru>
2021-05-09 14:27:44 +0300

    FirstDateOfWeekIso8601

---
+++ src/Common.Core/Extensions/DateTimeExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Common.Core.Extensions
+{
+    public static class DateTimeExtension
+    {
+        private const int _maxYear = 9999;
+
+        public static DateTime FirstDateOfWeekIso8601(this DateTime datetime, int year = _maxYear, int weekOfYear = 0) => FirstDateOfWeekIso8601(year == _maxYear ? datetime.Year : year, weekOfYear);
+
+        public static DateTime FirstDateOfWeekIso8601(int year, int weekOfYear)
+        {
+            //TODO: message
+            if (year is < 1 or > _maxYear)
+                throw new ArgumentException("the year should be in the range 1 to 9999");
+
+            if ((year - 1) * 52 <= weekOfYear)
+                throw new ArgumentException("Test");
+
+            var jan = new DateTime(year, 1, 1);
+            int daysOffset = DayOfWeek.Thursday - jan.DayOfWeek;
+
+            DateTime firstThursday = jan.AddDays(daysOffset);
+            var cal = CultureInfo.CurrentCulture.Calendar;
+            int firstWeek = cal.GetWeekOfYear(firstThursday, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+
+            if (firstWeek == 1)
+                weekOfYear -= 1;
+            
+            return firstThursday.AddDays(weekOfYear * 7 - 3);
+        }
+    }
+}
---
+++ test/Common.Core.Tests/Extensions/DateTimeExtensionTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Common.Core.Extensions;
+using Xunit;
+
+namespace Common.Core.Tests.Extensions
+{
+    public class DateTimeExtensionTest
+    {
+        [Theory]
+        [InlineData(2021, -1, 21)]
+        [InlineData(2021, 0, 28)]
+        [InlineData(2021, 1, 4)]
+        [InlineData(2021, 4, 25)]
+        [InlineData(2021, 52, 27)]
+        [InlineData(2021, 53, 3)]
+        public void FirstDateOfWeekIso8601_CalculateFirstDayOfWeekFromWeekAndYear_ReturnTrue(int year, int weekOfYear, int expected)
+        {
+            var result = DateTimeExtension.FirstDateOfWeekIso8601(year, weekOfYear);
+            Assert.Equal(expected, result.Day);
+        }
+
+        [Theory]
+        [InlineData(-2021, 0)]
+        [InlineData(0, 0)]
+        [InlineData(10000, 0)]
+        [InlineData(1, 0)]
+        [InlineData(1, 1)]
+        [InlineData(19999, 1)]
+        public void FirstDateOfWeekIso8601_CalculateFirstDayOfWeekFromWeekAndYear_ReturnArgumentException(int year, int weekOfYear)
+        {
+            void Act() => DateTimeExtension.FirstDateOfWeekIso8601(year, weekOfYear);
+            Assert.Throws<ArgumentException>(Act);
+        }
+    }
+}
