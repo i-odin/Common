48977c6be27953f8516f1f7c461581a1a2218a2d
b_a_x <ily-odinokov@yandex.ru>
2021-08-18 23:41:59 +0300

    CheckAttribute

---
+++ src/Common.Core/Extensions/EnumExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Common.Core.Extensions
+{
+    public static class EnumExtension
+    {
+        public static bool CheckAttribute(this Enum @enum, Type typeAttribute)
+        {
+            var result = false;
+            if (typeAttribute?.BaseType == typeof(Attribute))
+            {
+                var field = @enum.GetType().GetField(@enum.ToString());
+                if (field != null)
+                    result = field.IsDefined(typeAttribute, false);
+            }
+            return result;
+        }
+    }
+}
---
+++ test/Common.Core.Tests/Extensions/EnumExtensionTest.cs
@@ -0,0 +1,31 @@
+﻿using Common.Core.Extensions;
+using System;
+using Xunit;
+
+namespace Common.Core.Tests.Extensions
+{
+    public class EnumExtensionTest
+    {
+        [Theory]
+        [InlineData(EnumTest.One, typeof(TestAttribute), true)]
+        [InlineData(EnumTest.One, typeof(EnumExtensionTest), false)]
+        [InlineData(EnumTest.One, null, false)]
+        [InlineData(EnumTest.Two, typeof(TestAttribute), false)]
+        public void CheckAttribute_MultipleEnums_ReturnExpected(EnumTest inputOne, Type inputTwo, bool expected)
+        {
+            var result = inputOne.CheckAttribute(inputTwo);
+            Assert.Equal(expected, result);
+        }
+
+        public enum EnumTest
+        {
+            [Test]
+            One,
+            Two
+        }
+
+        public class TestAttribute : Attribute
+        {
+        }
+    }
+}
