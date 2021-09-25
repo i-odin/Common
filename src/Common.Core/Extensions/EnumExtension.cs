c3fea351a78f6dffbd63dc81d2dba414a4ebd5bb
b_a_x <ily-odinokov@yandex.ru>
2021-09-25 13:16:33 +0300

    Enum GetAttribute

--- src/Common.Core/Extensions/EnumExtension.cs
+++ src/Common.Core/Extensions/EnumExtension.cs
@@ -1,19 +1,33 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Common.Core.Extensions
 {
     public static class EnumExtension
     {
-        public static bool HasAttribute(this Enum @enum, Type typeAttribute)
+        public static bool HasAttribute<TAttr>(this Enum @enum) 
+            where TAttr : Attribute
         {
             var result = false;
-            if (typeAttribute?.BaseType == typeof(Attribute))
-            {
-                var field = @enum.GetType().GetField(@enum.ToString());
-                if (field != null)
-                    result = field.IsDefined(typeAttribute, false);
-            }
+            var field = @enum.GetType().GetField(@enum.ToString());
+            if (field != null)
+                result = field.IsDefined(typeof(TAttr), false);
+            
             return result;
+        }
+
+        [return: MaybeNull]
+        public static TAttr? GetAttribute<TAttr>(this Enum @enum)
+            where TAttr : Attribute
+        {    
+            var field = @enum.GetType().GetField(@enum.ToString());
+            if (field != null)
+            {
+                var attributes = field.GetCustomAttributes(typeof(TAttr), false);
+                if(attributes != null && attributes.Length > 0)
+                    return (TAttr)attributes[0];
+            }
+            return default;
         }
     }
 }
--- test/Common.Core.Tests/Extensions/EnumExtensionTest.cs
+++ test/Common.Core.Tests/Extensions/EnumExtensionTest.cs
@@ -7,25 +7,33 @@ namespace Common.Core.Tests.Extensions
     public class EnumExtensionTest
     {
         [Theory]
-        [InlineData(EnumTest.One, typeof(TestAttribute), true)]
-        [InlineData(EnumTest.One, typeof(EnumExtensionTest), false)]
-        [InlineData(EnumTest.One, null, false)]
-        [InlineData(EnumTest.Two, typeof(TestAttribute), false)]
-        public void HasAttribute_MultipleEnums_ReturnExpected(EnumTest inputOne, Type inputTwo, bool expected)
+        [InlineData(EnumTest.One, true)]
+        [InlineData(EnumTest.Two, false)]
+        public void HasAttribute_MultipleEnums_ReturnExpected(EnumTest input, bool expected)
         {
-            var result = inputOne.HasAttribute(inputTwo);
+            var result = input.HasAttribute<TestAttribute>();
             Assert.Equal(expected, result);
         }
 
-        public enum EnumTest
+        [Theory]
+        [InlineData(EnumTest.One, typeof(TestAttribute))]
+        [InlineData(EnumTest.Two, null)]
+        public void GetAttribute_MultipleEnums_ReturnExpected(EnumTest input, Type expected)
         {
-            [Test]
-            One,
-            Two
+            var result = input.GetAttribute<TestAttribute>();
+            Assert.Equal(expected, result?.GetType());
+        }
+
+        public enum EnumTest 
+        {
+            [Test2] [Test]  One,
+            [Test2] Two 
         }
 
         public class TestAttribute : Attribute
-        {
-        }
+        { }
+
+        public class Test2Attribute : Attribute
+        { }
     }
 }
