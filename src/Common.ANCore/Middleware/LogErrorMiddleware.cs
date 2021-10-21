9fbd05a2535645f20dae0a2985fea606b679d956
b_a_x <ily-odinokov@yandex.ru>
2021-10-21 13:04:06 +0300

    rename project aspnetcore -> ancore

---
+++ src/Common.ANCore/Middleware/LogErrorMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Common.Core.Helpers;
+using Common.Core.Wrappers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Threading.Tasks;
+using Common.Core.Models;
+using Common.Core.Utilities;
+
+namespace Common.AspNetCore.Middleware
+{
+    //TODO: Тест
+    public class LogErrorMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<LogErrorMiddleware> _logger;
+        private readonly ISerializerWrapper _serializer;
+
+        public LogErrorMiddleware([NotNull] RequestDelegate next, [NotNull] ILoggerFactory loggerFactory)
+        {
+            Throw.NotNull(next, nameof(next));
+            Throw.NotNull(loggerFactory, nameof(loggerFactory));
+
+            _next = next;
+            _logger = loggerFactory.CreateLogger<LogErrorMiddleware>();
+            _serializer = new JsonTextSerializerWrapper();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            _logger.LogError(exception, Errors.Message.InternalServer);
+            if (exception is AggregateException aggregateException && aggregateException.InnerExceptions.Count > 0)
+                foreach (Exception innerException in aggregateException.InnerExceptions)
+                    _logger.LogError(innerException, Errors.Message.InternalServerInnerException);
+
+            var error = new Error(nameof(HttpStatusCode.InternalServerError), exception.Message);
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return context.Response.WriteAsync(_serializer.Serialize(error));
+        }
+    }
+}
---
+++ src/Common.ANCore/Middleware/LogResponseMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Core.Helpers;
+using Common.Core.Utilities;
+using Common.Core.Extensions;
+using Common.Core.Structs;
+
+namespace Common.AspNetCore.Middleware
+{
+    //TODO: Test
+    public class LogResponseMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<LogResponseMiddleware> _logger;
+        private readonly LogLevel _logLevel;
+
+        public LogResponseMiddleware([NotNull] RequestDelegate next, [NotNull] ILoggerFactory loggerFactory)
+        {
+            Throw.NotNull(next, nameof(next));
+            Throw.NotNull(loggerFactory, nameof(loggerFactory));
+
+            _next = next;
+            _logger = loggerFactory.CreateLogger<LogResponseMiddleware>();
+            _logLevel = LogLevel.Information;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (_logger.IsEnabled(_logLevel))
+            {
+                Stream originalResponseBody = context.Response.Body;
+                var responseBodyStream = new MemoryStream();
+                context.Response.Body = responseBodyStream;
+                await _next(context);
+                responseBodyStream.Seek(0, SeekOrigin.Begin);
+                _logger.Log(_logLevel, MessageBuild(context, await new StreamReader(responseBodyStream).ReadToEndAsync()));
+                responseBodyStream.Seek(0, SeekOrigin.Begin);
+                await responseBodyStream.CopyToAsync(originalResponseBody);
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+
+        private static string MessageBuild(HttpContext context, string body)
+        {
+            var spanValues = new ReadOnlySpan<KeyValueString>(new[]
+            {
+                new KeyValueString(Messages.TraceIdentifier, context.TraceIdentifier),
+                new KeyValueString(Messages.Body, body)
+            });
+            return new StringBuilder(body.Length).AppendJoin(in spanValues).ToString();
+        }
+    }
+}
