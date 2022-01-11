6b0ba4e57d594f1b4fd3d2d54d19ddca94dd0632
b_a_x <ily-odinokov@yandex.ru>
2022-01-11 13:39:48 +0300

    EnumConverter

---
+++ test/Common.Core.Tests/Convertible/EnumConverterTest.cs
@@ -0,0 +1,28 @@
+﻿
+using Common.Core.Convertible;
+using Xunit;
+
+namespace Common.Core.Tests.Convertible
+{
+
+    public class EnumConverterTest
+    {
+        [Theory]
+        [InlineData(0, TestConvertEnum.Zero)]
+        [InlineData(1, TestConvertEnum.One)]
+        [InlineData(2, TestConvertEnum.Two)]
+        public void EnumConverter_ConvertIntToEnum_ReturnTrue(int value, TestConvertEnum expected)
+        {
+            var result = EnumConverter<TestConvertEnum>.Convert(value);
+            
+            Assert.Equal(expected, result);
+        }
+
+        public enum TestConvertEnum
+        {
+            Zero = 0,
+            One = 1,
+            Two = 2
+        }
+    }
+}
