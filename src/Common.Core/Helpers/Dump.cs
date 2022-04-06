ad9ee42b1067345352dd5aa39ae3daa9abf10136
b_a_x <ily-odinokov@yandex.ru>
2022-04-06 12:31:01 +0300

    orderWeight

--- src/Common.Core/Helpers/Dump.cs
+++ src/Common.Core/Helpers/Dump.cs
@@ -6,4 +6,17 @@ internal class Dump
     {
         return long.Parse(string.Concat(numbers)).ToString("(000) 000-0000");
     }
+
+    /// <summary>
+    /// Ordered by numbers weights
+    /// </summary>
+    /// <param s="56 65 74 100 99 68 86 180 90"></param>
+    /// <returns>"100 180 90 56 65 74 68 86 99"</returns>
+    public static string orderWeight(string s)
+    {
+        return string.Join(" ", s.Split(' ')
+            .OrderBy(n => n.ToCharArray()
+            .Select(c => (int)char.GetNumericValue(c)).Sum())
+            .ThenBy(n => n));
+    }
 }
