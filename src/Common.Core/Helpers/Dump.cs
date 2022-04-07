d01497e79e444060506af925f55817e903705f83
b_a_x <ily-odinokov@yandex.ru>
2022-04-07 16:47:27 +0300

    dump

--- src/Common.Core/Helpers/Dump.cs
+++ src/Common.Core/Helpers/Dump.cs
@@ -1,4 +1,6 @@
-﻿namespace Common.Core.Helpers;
+﻿using System.Text.RegularExpressions;
+
+namespace Common.Core.Helpers;
 
 internal class Dump
 {
@@ -10,7 +12,7 @@ internal class Dump
     /// <summary>
     /// Ordered by numbers weights
     /// </summary>
-    /// <param s="56 65 74 100 99 68 86 180 90"></param>
+    /// <param name="s">"56 65 74 100 99 68 86 180 90"</param>
     /// <returns>"100 180 90 56 65 74 68 86 99"</returns>
     public static string orderWeight(string s)
     {
@@ -19,4 +21,41 @@ internal class Dump
             .Select(c => (int)char.GetNumericValue(c)).Sum())
             .ThenBy(n => n));
     }
-}
+
+    /// <summary>
+    /// Get count smileys
+    /// </summary>
+    /// <param name="smileys">"[':)', ';(', ';}', ':-D']"</param>
+    /// <returns>2</returns>
+    public static int CountSmileys(string[] smileys) => 
+        smileys.Count(s => Regex.IsMatch(s, @"^[:;]{1}[~-]{0,1}[\)D]{1}$"));
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="str">"the-stealth-warrior"</param>
+    /// <returns>"theStealthWarrior"</returns>
+    public static string ToCamelCase(string str) =>
+        Regex.Replace(str, @"[_-](\w)", m => m.Groups[1].Value.ToUpper());
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="iterable">"AAAABBBCCDAABBB"</param>
+    /// <returns>{'A', 'B', 'C', 'D', 'A', 'B'}</returns>
+    public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+    {
+            if (iterable == null) yield return (T)Enumerable.Empty<T>();
+
+            T? previous = default;
+            foreach (var current in iterable)
+            {
+                if (current?.Equals(previous) == false)
+                {
+                    previous = current;
+                    yield return current;
+                }
+            }
+        }
+    }
