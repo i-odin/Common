96b46e8b77d77b92e8be44e54e171b16ae343605
b_a_x <ily-odinokov@yandex.ru>
2022-05-23 17:51:35 +0300

    tree

---
+++ src/Common.Core/Trees/Node.cs
@@ -0,0 +1,18 @@
+﻿namespace Common.Core.Trees
+{
+    public class Node<TData>
+    {
+        public TData Data { get; set; }
+    }
+
+    public class TreeNode<TData> : Node<TData>
+    {
+        public ICollection<TreeNode<TData>> Nodes { get; set; }
+    }
+
+    public class BinaryNode<TData> : Node<TData>
+    {
+        public BinaryNode<TData> Left { get; set; }
+        public BinaryNode<TData> Right { get; set; }
+    }
+}
---
+++ src/Common.Core/Trees/Tree.cs
@@ -0,0 +1,119 @@
+﻿namespace Common.Core.Trees
+{
+    public class Tree<TNode>
+    {
+        public TNode Root { get; set; }
+    }
+
+    public class BinaryTree : Tree<BinaryNode<int>>
+    {
+        /// <summary>
+        /// Обход в глубину: сверху-вниз
+        /// </summary>
+        void PreOrder(BinaryNode<int> node)
+        {
+            if (node != null)
+            {
+                Procedyre(node);
+                PreOrder(node.Left);
+                PreOrder(node.Right);
+            }
+        }
+
+        /// <summary>
+        /// Обход в глубину: слева-направо
+        /// </summary>
+        void InOrder(BinaryNode<int> node)
+        {
+            if (node != null)
+            {
+                PreOrder(node.Left);
+                Procedyre(node);
+                PreOrder(node.Right);
+            }
+        }
+
+        /// <summary>
+        /// Обход в глубину: снизу-вверх
+        /// </summary>
+        void PostOrder(BinaryNode<int> node)
+        {
+            if (node != null)
+            {
+                PreOrder(node.Left);
+                PreOrder(node.Right);
+                Procedyre(node);
+            }
+        }
+
+        /// <summary>
+        /// Обработка дерева
+        /// </summary>
+        /// <param name="node"></param>
+        void Procedyre(BinaryNode<int> node)
+        {
+        }
+
+        /// <summary>
+        /// Вставить элемент в упорядочное дерево
+        /// </summary>
+        void Insert(ref BinaryNode<int> current, BinaryNode<int> node)
+        {
+            if (current == null)
+                current = node;
+            else if (current.Data <= node.Data)
+            {
+                var left = current.Left;
+                Insert(ref left, node);
+            }
+            else
+            {
+                var rigth = current.Right;
+                Insert(ref rigth, node);
+            }
+        }
+
+        void Delete(int data, ref BinaryNode<int> tree)
+        {
+            if (tree != null)
+            {
+                if (data < Convert.ToInt32(tree.Data))
+                {
+                    var left = tree.Left;
+                    Delete(data, ref left);
+                }
+                else if (data > Convert.ToInt32(tree.Data))
+                {
+                    var right = tree.Right;
+                    Delete(data, ref right);
+                }
+                else
+                {
+                    var q = tree;
+                    if (q.Right == null)
+                        tree = q.Left;
+                    else if (q.Left == null)
+                        tree = q.Right;
+                    else
+                    {
+                        var left = q.Left;
+                        Del(ref left, ref left);
+                    }
+                }
+            }
+            void Del(ref BinaryNode<int> r, ref BinaryNode<int> parent)
+            {
+                if (r.Right != null)
+                {
+                    var right = r.Right;
+                    var q = parent;
+                    Del(ref right, ref q);
+                }
+                else
+                {
+                    parent.Data = r.Data; parent = r; r = r.Left;
+                }
+            }
+        }
+    }
+}
