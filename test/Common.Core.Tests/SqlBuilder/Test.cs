51b865b61d16405c41eebc17cb71eba136668163
Илья <ily-odinokov@yandex.ru>
2023-04-23 12:22:59 +0300

    UpdateWhere_BuildUpdateWhereSql

--- test/Common.Core.Tests/SqlBuilder/Test.cs
+++ test/Common.Core.Tests/SqlBuilder/Test.cs
@@ -24,9 +24,26 @@ set Id = '00000000-0000-0000-0000-000000000000', Name = null, Age = 10, Timespan
 
         [Theory]
         [InlineData("where Id = '00000000-0000-0000-0000-000000000000', Name = null, Age = 10, Timespan = '2023-04-23T00:00:00.0000000'")]
-        public void Where_BuildUpdateSql(string expected)
+        public void Where_BuildWereSql(string expected)
         {
             var builder = new MsSqlBuilder().Where<TestClass>(x => x.Set(y => y.Id, Guid.Empty)
+                                                                    .Set(y => y.Name, null)
+                                                                    .Set(y => y.Age, 10)
+                                                                    .Set(y => y.Timespan, new DateTime(2023, 04, 23)));
+            Assert.Equal(expected, builder.ToString());
+        }
+
+        [Theory]
+        [InlineData(@"update TestClass 
+set Id = '00000000-0000-0000-0000-000000000000', Name = null, Age = 10, Timespan = '2023-04-23T00:00:00.0000000'
+where Id = '00000000-0000-0000-0000-000000000000', Name = null, Age = 10, Timespan = '2023-04-23T00:00:00.0000000'")]
+        public void UpdateWhere_BuildUpdateWhereSql(string expected)
+        {
+            var builder = new MsSqlBuilder().Update<TestClass>(x => x.Set(y => y.Id, Guid.Empty)
+                                                                     .Set(y => y.Name, null)
+                                                                     .Set(y => y.Age, 10)
+                                                                     .Set(y => y.Timespan, new DateTime(2023, 04, 23)))
+                                            .Where<TestClass>(x => x.Set(y => y.Id, Guid.Empty)
                                                                     .Set(y => y.Name, null)
                                                                     .Set(y => y.Age, 10)
                                                                     .Set(y => y.Timespan, new DateTime(2023, 04, 23)));
