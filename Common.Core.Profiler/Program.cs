c4f9a9cfce443c94a95ba78fa69a188982b680e1
b_a_x <ily-odinokov@yandex.ru>
2023-05-15 19:49:29 +0300

    fix

--- Common.Core.Profiler/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Common.Core.Profiler.SqlBuilder.Ms;
-
-Console.WriteLine("Enter any character");
-Console.ReadKey();
-
-MsSqlBuilderProfiler.Run();
-
-Console.WriteLine("End");
-Console.ReadKey();
